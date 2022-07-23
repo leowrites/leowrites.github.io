50d95c3d2536105787f2dace49f7b71a9726a18d
leowrites <75044178+leowrites@users.noreply.github.com>
2022-07-23 19:49:29 -0400

    Changed scroll effect

--- src/TitleText/TitleText.jsx
+++ src/TitleText/TitleText.jsx
@@ -2,51 +2,38 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./TitleText.css";
 
-export default function ({
-  textShadow1 = "0 0 0.5rem white, 0 0 1em #0ff, 0 0 0.5em  #f0f",
-  textShadow2 = "0 0 0.5rem white, 0 0 1em #00abff, 0 0 0.5em #1fff00",
-  titleSectionRef,
-}) {
-  const mainTl = useRef();
+export default function ({ titleSectionRef, canvaContainerRef }) {
   const glowTl = useRef();
   const titleRef = useRef();
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    const pinTextAnimation = gsap.to(titleSectionRef.current, {
-      scrollTrigger: {
-        trigger: titleSectionRef.current,
-        pin: titleSectionRef.current,
-        start: "top top",
-        scrub: 1,
-        markers: true,
-      },
-      scale: 0,
-    });
     glowTl.current = gsap
       .timeline()
-      .fromTo(
+      .to(
+        titleSectionRef.current,
+        {
+          opacity: 0,
+          transform: "scale(200)",
+          zIndex: -1,
+        },
+        ">3"
+      )
+      .to(
         titleRef.current,
         {
-          textShadow: "0 0 0 white",
+          zIndex: -1,
         },
-        {
-          duration: 2,
-          textShadow: textShadow1,
-        }
+        "<"
       )
-      .to(titleRef.current, {
-        duration: 5,
-        repeat: -1,
-        yoyo: true,
-        textShadow: textShadow2,
-      });
-    mainTl.current = gsap.timeline().add(glowTl.current).add(pinTextAnimation);
+      .to(
+        canvaContainerRef.current,
+        {
+          zIndex: 999,
+        },
+        ">"
+      );
     return () => {
-      pinTextAnimation.kill();
-      mainTl.current.kill();
       glowTl.current.kill();
     };
   }, []);
@@ -57,14 +44,15 @@ export default function ({
         height: "100vh",
         width: "100vw",
         display: "flex",
-        justifyContent: "center",
         alignItems: "center",
+        justifyContent: "center",
+        zIndex: 1,
       }}
-      textAlign="center"
     >
       <Typography
         id="title-text"
         sx={{
+          zIndex: 1,
           fontSize: "12rem",
         }}
         color={"white"}
