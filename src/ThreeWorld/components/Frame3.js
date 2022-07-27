f3fa97687e2e9dfde1509b5f8348b43d004a7e08
leowrites <75044178+leowrites@users.noreply.github.com>
2022-07-27 13:53:26 -0400

    Fixed assets not loading

--- src/ThreeWorld/components/Frame3.js
+++ src/ThreeWorld/components/Frame3.js
@@ -11,9 +11,7 @@ import { useGLTF } from "@react-three/drei";
 
 export default function Model({ ...props }) {
   const group = useRef();
-  const { nodes, materials } = useGLTF(
-    process.env.PUBLIC_URL + "/Frame3/scene.gltf"
-  );
+  const { nodes, materials } = useGLTF("/Frame3/scene.gltf");
   return (
     <group ref={group} {...props} dispose={null}>
       <group>
