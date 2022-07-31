ddb24db4654285f150d3434404019e4898b8bf83
leowrites <75044178+leowrites@users.noreply.github.com>
2022-07-30 22:19:53 -0400

    Migrate to Spline

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import "App.css";
-// import FeedbackForm from "FeedbackForm/FeedbackForm";
-import TitleText from "TitleText/TitleText";
 import ThreeWorld from "ThreeWorld/ThreeWorld";
 import Navbar from "Navbar/Navbar";
-import React, { useEffect, useRef } from "react";
-import { gsap } from "gsap";
+import React from "react";
 
 function App() {
-  const canvaContainerRef = useRef();
-  const titleSectionRef = useRef();
-  const canvasRef = useRef();
   return (
     <div className="App">
       <Navbar />
-      <ThreeWorld
-        titleSectionRef={titleSectionRef}
-        canvaContainerRef={canvaContainerRef}
-        canvasRef={canvasRef}
-      />
-      <TitleText
+      <ThreeWorld />
+      {/* <TitleText
         canvaContainerRef={canvaContainerRef}
         titleSectionRef={titleSectionRef}
-      />
-      {/* <FeedbackForm /> */}
-      <div style={{ height: "100vh" }}></div>
+      /> */}
     </div>
   );
 }
