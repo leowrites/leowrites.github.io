baae26f7f6ef764af2b0d23de106af9f86e07c1d
leowrites <75044178+leowrites@users.noreply.github.com>
2022-08-05 16:02:19 -0400

    Added fallback for background canvas

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import HomePage from "Home/HomePage";
 import { useSelector } from "react-redux";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { useProgress } from "@react-three/drei";
 
 function App() {
   const view = useSelector((state) => state.view.view);
-  console.log(view);
   return (
     <div
       className="App"
       style={{ backgroundColor: view === "start" ? "black" : "#f4f3ee" }}
     >
+      <Navbar />
       <Suspense fallback={<Loader />}>
-        <Navbar />
         <HomePage />
-        <Box sx={{ mb: "5rem" }}>
-          <Typography fontSize={"5rem"}>Welcome to my world</Typography>
+        <Box>
+          <Typography variant={"h2"} sx={{ my: "2rem", color: "black" }}>
+            Welcome to my world
+          </Typography>
         </Box>
         <ThreeWorld />
       </Suspense>
--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -7,19 +7,27 @@ import AboutMe from "./Sections/AboutMe";
 import Education from "./Sections/Education";
 import Projects from "./Sections/Projects";
 import Background from "./Background/Background";
+import Container from "@mui/material/Container";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import { Suspense } from "react";
 
 function HomeWrapper({ children }) {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(matches);
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        mx: "10rem",
-      }}
-    >
-      <Stack>{children}</Stack>
-    </Box>
+    <Container maxWidth={matches ? "sm" : "lg"}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Stack>{children}</Stack>
+      </Box>
+    </Container>
   );
 }
 
@@ -28,14 +36,14 @@ export default function HomePage() {
   return view === "start" ? (
     <MockLoader />
   ) : (
-    <>
-      <HomeWrapper>
-        <PageOne />
-        <AboutMe />
-        <Education />
-        <Projects />
-      </HomeWrapper>
-      <Background />
-    </>
+    <HomeWrapper>
+      <PageOne />
+      <AboutMe />
+      <Education />
+      <Projects />
+      <Suspense fallback={null}>
+        <Background />
+      </Suspense>
+    </HomeWrapper>
   );
 }
