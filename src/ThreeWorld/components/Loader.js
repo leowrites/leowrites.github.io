49811b22ab64cd1f7b699bb7bb0feb34ca34d222
leowrites <75044178+leowrites@users.noreply.github.com>
2022-08-05 13:12:49 -0400

    Added loading bar

--- src/ThreeWorld/components/Loader.js
+++ src/ThreeWorld/components/Loader.js
@@ -1,21 +1,37 @@
+import React, { useRef } from "react";
 import { useProgress } from "@react-three/drei";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import "./Loader.css";
+
 export default function Loader() {
-  const { progress, item, loaded, total } = useProgress();
-  console.log(item, loaded, total);
+  const { progress } = useProgress();
+  const loadingBarRef = useRef();
+  console.log(progress);
   return (
     <Box id={"loader"}>
-      <Stack>
+      <Stack spacing={2}>
         <Typography sx={{ color: "white", fontSize: "8rem" }}>
           Siqi Liu
         </Typography>
+        <LoaderBar ref={loadingBarRef} width={`${progress}%`} />
         <Typography sx={{ color: "white", fontSize: "2rem" }}>
-          Loading... {progress}%
+          Loading... {Math.round(progress)}%
         </Typography>
+        <Box sx={{ width: "5rem", height: "5rem" }} />
       </Stack>
     </Box>
   );
 }
+
+export const LoaderBar = React.forwardRef((props, ref) => (
+  <Box
+    ref={ref}
+    sx={{
+      backgroundColor: "white",
+      height: "1rem",
+      ...props,
+    }}
+  />
+));
