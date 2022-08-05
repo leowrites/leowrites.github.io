6d6b8c6edb023103874080fb6509bdbdc704c537
leowrites <75044178+leowrites@users.noreply.github.com>
2022-08-05 13:10:24 -0400

    Added homepage to app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import "App.css";
 import ThreeWorld from "ThreeWorld/ThreeWorld";
 import Navbar from "Navbar/Navbar";
 import React from "react";
+import Loader from "ThreeWorld/components/Loader";
+import { Suspense } from "react";
+import HomePage from "Home/HomePage";
+import { useSelector } from "react-redux";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const view = useSelector((state) => state.view.view);
+  console.log(view);
   return (
-    <div className="App">
-      <Navbar />
-      <ThreeWorld />
-      {/* <TitleText
-        canvaContainerRef={canvaContainerRef}
-        titleSectionRef={titleSectionRef}
-      /> */}
+    <div
+      className="App"
+      style={{ backgroundColor: view === "start" ? "black" : "#f4f3ee" }}
+    >
+      <Suspense fallback={<Loader />}>
+        <Navbar />
+        <HomePage />
+        <Box sx={{ mb: "5rem" }}>
+          <Typography fontSize={"5rem"}>Welcome to my world</Typography>
+        </Box>
+        <ThreeWorld />
+      </Suspense>
     </div>
   );
 }
