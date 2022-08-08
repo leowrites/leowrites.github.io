62f9bd758339b867591300b138fddf27041a7705
leowrites <75044178+leowrites@users.noreply.github.com>
2022-08-08 19:28:44 -0400

    Removed ThreeWorld from App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import "App.css";
-import ThreeWorld from "ThreeWorld/ThreeWorld";
+// import ThreeWorld from "ThreeWorld/ThreeWorld";
 import Navbar from "Navbar/Navbar";
 import React from "react";
-import Loader from "ThreeWorld/components/Loader";
-import { Suspense } from "react";
 import HomePage from "Home/HomePage";
 import { useSelector } from "react-redux";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { useProgress } from "@react-three/drei";
 
 function App() {
   const view = useSelector((state) => state.view.view);
   return (
     <div
       className="App"
-      style={{ backgroundColor: view === "start" ? "black" : "#f4f3ee" }}
+      style={{ backgroundColor: view === "start" ? "black" : "#051E36" }}
     >
       <Navbar />
-      <Suspense fallback={<Loader />}>
-        <HomePage />
-        <Box>
-          <Typography variant={"h2"} sx={{ my: "2rem", color: "black" }}>
-            Welcome to my world
+      <HomePage />
+      {/* {matches ? (
+          <>
+            <Box>
+              <Typography variant={"h2"} sx={{ py: "5rem", color: "white" }}>
+                Welcome to my world
+              </Typography>
+            </Box>
+            <ThreeWorld />
+          </>
+        ) : (
+          <Typography variant={"h6"} sx={{ py: "5rem", color: "white" }}>
+            Visit this website on a desktop to see my world!
           </Typography>
-        </Box>
-        <ThreeWorld />
-      </Suspense>
+        )} */}
     </div>
   );
 }
